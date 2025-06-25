da67ba0d238f56791a62e9b4736bf42a51a8b300
BatLeDev <baptiste.guerny@gmail.com>
2025-06-25 16:45:06 +0200

    feat: throw an error when apiKey isn't valid

--- lib/prepare.ts
+++ lib/prepare.ts
@@ -2,9 +2,12 @@ import type { PrepareContext } from '@data-fair/lib-common-types/catalog/index.j
 import type { UDataCapabilities } from './capabilities.ts'
 import type { UDataConfig } from '#types'
 
-export default async ({ catalogConfig, capabilities, secrets }: PrepareContext<UDataConfig, UDataCapabilities>) => {
-  const apiKey = catalogConfig.apiKey
+import axios from '@data-fair/lib-node/axios.js'
+import { httpError } from '@data-fair/lib-utils/http-errors.js'
 
+export default async ({ catalogConfig, capabilities, secrets }: PrepareContext<UDataConfig, UDataCapabilities>) => {
+  // Manage secrets
+  const apiKey = catalogConfig.apiKey
   // If the config contains a secretField, and it is not already hidden
   if (apiKey && apiKey !== '**************************************************') {
     // Hide the secret in the catalogConfig, and copy it to secrets
@@ -17,9 +20,33 @@ export default async ({ catalogConfig, capabilities, secrets }: PrepareContext<U
     delete secrets.apiKey
   }
 
+  // Manage capabilities
   if (secrets?.apiKey) {
     if (!capabilities.includes('publishDataset')) capabilities.push('publishDataset')
   } else capabilities = capabilities.filter(c => c !== 'publishDataset')
+
+  // Check if the APIkey is valid by getting the user info
+  if (secrets?.apiKey) {
+    try {
+      const user = (await axios.get(`${catalogConfig.url}/api/1/me`, {
+        headers: {
+          'X-API-KEY': secrets.apiKey
+        }
+      })).data
+
+      // If they are an organization, check if the user has the right on this organization
+      if (catalogConfig.organization?.id) {
+        if (!user.organizations || !Array.isArray(user.organizations)) throw httpError(403, 'User has no organizations')
+        if (!user.organizations.some((org: any) => org.id === catalogConfig.organization!.id)) {
+          throw httpError(403, `User does not have access to organization ${catalogConfig.organization.id}`)
+        }
+      }
+    } catch (error: any) {
+      console.log(error)
+      if (error.status === 401) throw httpError(401, 'Invalid API key')
+      throw httpError(error.status || 500, `API validation failed: ${error.message || 'Unknown error'}`)
+    }
+  }
 
   return {
     catalogConfig,
